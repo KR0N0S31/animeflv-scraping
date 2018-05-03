eb40652a969631f8df67b6465a16635722318b88
Felipe Martinez <andresfelipe.2031@gmail.com>
2018-05-03 00:19:26 -0500

    Creado sistema de actualizacion de la base de datos

--- db/actions.py
+++ db/actions.py
@@ -1,5 +1,7 @@
 import peewee as pw
 from scraping.main import AnimeScraping
+from scraping.main import FactoryListAnime as fla
+from db.settings import DATABASE 
 from .models import Genre, Anime, AnimeGenre, AnimeRelation, Episode, State, AnimeState
 
 
@@ -32,20 +34,137 @@ class DataBase:
         anime_record.save()
         for i in anime.genres:
             genre, created = Genre.get_or_create(genre=i)
-            animeGenre_record = AnimeGenre.create(anime=anime_record, genre=genre)
-            animeGenre_record.save()
-        
+            AnimeGenre.create(anime=anime_record, genre=genre).save()
         state, created = State.get_or_create(state=anime.state)
-        animeState_record = AnimeState.create(anime=anime_record, state=state)
-        animeState_record.save()
+        AnimeState.create(anime=anime_record, state=state).save()
 
         for i in anime.animeRel_to_db():
             i['anime'] = anime_record
-            animeRelation_record = AnimeRelation.create(**i)
-            animeRelation_record.save()
+            AnimeRelation.create(**i).save()
         for i in anime.episode_list_to_db():
             i['anime'] = anime_record
-            episode_record = Episode.create(**i)
-            episode_record.save()
+            Episode.create(**i).save()
+    
+    @staticmethod
+    def update_anime(anime):
+    
+        if not isinstance(anime, Anime):
+            raise TypeError("anime must be a Anime")
+
+        anime_data = fla.get_anime(anime.url)
+
+        if anime.aid != anime_data.aid:
+            anime.aid = anime_data.aid
+        if anime.slug != anime_data.slug:
+            anime.slug = anime_data.slug
+        if anime.url != anime_data.url:
+            anime.url = anime_data.url
+        if anime.name != anime_data.name:
+            anime.name = anime_data.name
+        if anime.anime_type != anime_data.anime_type:
+            anime.anime_type = anime_data.anime_type
+        if anime.image != anime_data.image:
+            anime.image = anime_data.image
+        if anime.synopsis != anime_data.synopsis:
+            anime.synopsis = anime_data.synopsis
+
+        anime.save()
+
+        def update_genres(anime, anime_data):
+            AnimeGenre.delete().where(AnimeGenre.anime == anime).execute()
+            for i in anime_data.genres:
+                genre, created = Genre.get_or_create(genre=i)
+                AnimeGenre.create(anime=anime, genre=genre).save()
+        query = (AnimeGenre
+            .select()
+            .where(AnimeGenre.anime == anime)
+        )
+        if len(query) != len(anime_data.genres):
+            update_genres(anime, anime_data)
+        else:
+            for i in range(len(query)):
+                if query[i].genre.genre != anime_data.genres[i]:
+                    update_genres(anime, anime_data)
+                    break
+
+
+        def update_state(anime, anime_data):
+            AnimeState.delete().where(AnimeState.anime == anime).execute()
+            state, created = State.get_or_create(state=anime_data.state)
+            AnimeState.create(anime=anime, state=state).save()
         
+        query = AnimeState.get(AnimeState.anime == anime)
+        if query.state.state != anime_data.state:
+            update_state(anime, anime_data)
+
+
+        def update_relations(anime, anime_data):
+            AnimeRelation.delete().where(AnimeRelation.anime == anime).execute()
+            for i in anime_data.animeRel_to_db():
+                i['anime'] = anime
+                AnimeRelation.create(**i).save()
+
+        query = (AnimeRelation
+            .select()
+            .where(AnimeRelation.anime == anime)
+        )
+        if len(query) != len(anime_data.listAnmRel):
+            update_relations(anime, anime_data)
+        else:
+            for i in range(len(query)):
+                if (
+                    query[i].aid != anime_data.listAnmRel[i].aid or 
+                    query[i].rel != anime_data.listAnmRel[i].rel or
+                    query[i].url != anime_data.listAnmRel[i].url
+                    ):
+                    update_relations(anime, anime_data)
+                    break
+
+
+        def update_episodes(anime, anime_data):
+            Episode.delete().where(Episode.anime == anime).execute()
+            for i in anime_data.episode_list_to_db():
+                i['anime'] = anime
+                Episode.create(**i).save()
+
+        query = (Episode
+            .select()
+            .where(Episode.anime == anime)
+        )
+        if len(query) != len(anime_data.episode_list):
+            update_episodes(anime, anime_data)
+        else:
+            for i in range(len(query)):
+                if (
+                    query[i].name != anime_data.episode_list[i].name or 
+                    query[i].url != anime_data.episode_list[i].url or
+                    query[i].image != anime_data.episode_list[i].image
+                    ):
+                    update_episodes(anime, anime_data)
+                    break
         
+    @staticmethod
+    def update_animes_in_emision():
+        query = (Anime
+            .select()
+            .join(AnimeState)
+            .join(State)
+            .where(State.state == 'En emision')
+        )
+        with DATABASE.atomic():
+            for i in query:
+                DataBase.update_anime(i)
+
+    @staticmethod
+    def update_database():
+        links = fla.get_all_animes()
+        with open('animes.txt', 'w') as text_file:
+            with DATABASE.atomic():
+                for i in links:
+                    anime = fla.get_anime(i)
+                    text_file.write(i+"\n")
+                    try:
+                        anime = Anime.get(aid=anime.aid)
+                        DataBase.update_anime(anime)
+                    except:
+                        DataBase.create_anime_record(anime)
--- main.py
+++ main.py
@@ -1,25 +1,8 @@
-from db.models import Anime, AnimeState, State
+from db.models import Anime, AnimeState, State, AnimeGenre, Genre
 from db.actions import DataBase
 from scraping.main import FactoryListAnime as fla
 
-def create_all_animes_records():
-    links = fla.get_all_animes()
-    with open('animes.txt', 'w') as text_file:
-        for i in links:
-            anime = fla.get_anime(i)
-            text_file.write(i+"\n")
-            try:
-                Anime.get(aid=anime.aid)
-            except:
-                DataBase.create_anime_record(anime)
 
 if __name__ == '__main__':
     DataBase.create_tables()
-    create_all_animes_records()
-
-    # query = (Anime
-    #         .select()
-    #         .join(AnimeGenre)
-    #         .join(Genre)
-    #         .where(Genre.genre == 'a')
-    #      )
+    DataBase.update_database()
